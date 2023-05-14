f9b1987a1e8935141a4e8ad485280da10015d4ad
alegorosito2 <132532293+alegorosito2@users.noreply.github.com>
2023-05-14 20:05:29 -0300

    Add files via upload

---
+++ puntero con funciones.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void cargar(int *p, int longitud){
+	for(int i = 0; i<longitud; i++){
+		p[i] = i*i;
+	}
+}
+	
+void mostrar(int *p, int longitud){
+	for(int i = 0; i<longitud; i++){
+		printf("%d\n", *(p+i));
+	}
+}
+int main(int argc, char *argv[]) {
+
+	int *p;
+	long int longitud;
+	printf("Cuantos elementos desea cargar: ");
+	scanf("%ld", &longitud);
+	
+	p = malloc(longitud * sizeof(int));
+	
+	cargar(p, longitud);
+	mostrar(p, longitud);
+	free(p);
+	return 0;
+}
+
